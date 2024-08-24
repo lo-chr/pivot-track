1f445d6ed2d0dc63431ceebca4d77dae0b97c658
Christoph Lobmeyer <lochr+gitcommit@posteo.de>
2024-08-24 16:49:50 +0200

    test: Add initial unit tests for `utils.load_config()`

---
+++ tests/utils_test.py
@@ -0,0 +1,28 @@
+from  pivot_track.lib import utils
+from pathlib import Path
+
+import pytest
+import uuid
+
+class TestLoadConfig:
+    def test_empty_string(self):
+        with pytest.raises(AttributeError) as e:
+            utils.load_config("")
+        assert str(e.value) == "Configuration path only allows Path objects"
+
+    def test_path_none(self):
+        with pytest.raises(FileNotFoundError) as e:
+            utils.load_config(None)
+        assert type(e) == FileNotFoundError
+    
+    def test_path_not_exist(self):
+        with pytest.raises(FileNotFoundError) as e:
+            path = Path(Path.cwd() / str(uuid.uuid4()))
+            utils.load_config(path)
+        assert type(e) == FileNotFoundError
+    
+    def test_file_exists_wrong_format(self):
+        assert type(utils.load_config(Path.cwd() / "README.md")) == None
+    
+    def test_file_exists_right_format(self):
+        assert type(utils.load_config(Path.cwd() / "config" / "config.example.yaml")) == dict

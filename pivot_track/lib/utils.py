9346c4c4e6fbcdec10afaf8244fabf5f2b02dddf
Christoph Lobmeyer <lochr+gitcommit@posteo.de>
2024-08-24 14:36:02 +0200

    docs: Add docstrings to `utils` module

--- pivot_track/lib/utils.py
+++ pivot_track/lib/utils.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import yaml
 
 
-def load_config(path:Path = None):
+def load_config(path:Path = None) -> dict:
+    """This function is loads the configuration from a file and returns it."""
     if(path == None):
         path = str(Path.home() / '.pivottrack' / 'config.yaml')
     
@@ -14,10 +15,12 @@ def load_config(path:Path = None):
 
 # TODO Rename in find_connector_class_by_name
 def find_connector_class(parent_class, name:str):
+    """This function returns a sub class of a parent class."""
     for connector in parent_class.__subclasses__():
         if name in connector.__name__.lower():
             return connector 
     return None
 
 def connector_classes_by_parent(parent_class):
+    """This function returns all sub classes of a given parent class."""
     return parent_class.__subclasses__()

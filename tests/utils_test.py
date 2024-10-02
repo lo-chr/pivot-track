33dcf9f3aacbb5a4bb2c291af6eb95dbff3bfe1f
Christoph Lobmeyer <lochr+gitcommit@posteo.de>
2024-10-02 19:11:10 +0200

    style: Fix ruff formatting

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,26 +1,31 @@
-from  pivot_track.lib import utils
+from pivot_track.lib import utils
 from pathlib import Path
 
 import pytest
 import uuid
 
+
 class TestLoadConfig:
     def test_empty_string(self):
         with pytest.raises(AttributeError) as e:
             utils.load_config("")
-        assert str(e.value) == "Pivot Track configuration path only allows Path objects."
+        assert (
+            str(e.value) == "Pivot Track configuration path only allows Path objects."
+        )
 
     def test_path_none(self):
         with pytest.raises(AttributeError):
             utils.load_config(None)
-    
+
     def test_path_not_exist(self):
         with pytest.raises(FileNotFoundError):
             path = Path(Path.cwd() / str(uuid.uuid4()))
             utils.load_config(path)
-    
+
     def test_file_exists_wrong_format(self):
         assert utils.load_config(Path.cwd() / "README.md") is None
-    
+
     def test_file_exists_right_format(self):
-        assert isinstance(utils.load_config(Path.cwd() / "config" / "config.example.yaml"), dict)
+        assert isinstance(
+            utils.load_config(Path.cwd() / "config" / "config.example.yaml"), dict
+        )
